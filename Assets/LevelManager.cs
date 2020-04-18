a1c8d88cac87a57c576181e1b7c059c8bcae9281
Ezra Szanton <ezraszanton@yahoo.com>
2020-04-18 11:29:10 -0400

    created on destroy
    now you always have the max number on screen!

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -9,21 +9,36 @@ public class LevelManager : MonoBehaviour
 
     public int maxPlayers;
 
+    public List<GameObject> players;
+
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < maxPlayers; i++)
+/*        for(int i = 0; i < maxPlayers; i++)
         {
             Instantiate(player, transform.position, Quaternion.identity);
-        }
+        }*/
     }
 
     // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (players[i] == null)
+            {
+                players.RemoveAt(i);
+            }
+        }
+
+        if (players.Count < maxPlayers)
+        {
+            players.Add(Instantiate(player, transform.position, Quaternion.identity));
+        }
+/*
         if (Input.GetKeyDown(KeyCode.R))
         {
             Instantiate(player, transform.position, Quaternion.identity);
-        }
+        }*/
     }
 }

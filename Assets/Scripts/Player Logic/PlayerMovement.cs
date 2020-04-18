11fb5ae8228a50c5e2d19c00c6237889584c999b
Ezra Szanton <ezraszanton@yahoo.com>
2020-04-17 21:41:28 -0400

    Player!

---
+++ Assets/Scripts/Player Logic/PlayerMovement.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject dust;
+
+    public bool jumpOverride;
+
+    [Tooltip("Speed the player travels upwards while jumping")]
+    public float jump_impulse;
+    [Tooltip("Horizontal Speed")]
+    public float speed;
+    [Tooltip("Rigidbody that controls this object (serialized to save a call to GameObject.Find())")]
+    public Rigidbody2D body;
+
+    //[Tooltip("Time before impact in which a jump input will still register")]
+    //public float jump_buffer;
+    bool jump_is_buffered;
+
+    [Tooltip("Detects ground for the purpose of jumping")]
+    public Transform right_foot;
+    [Tooltip("Detects ground for the purpose of jumping")]
+    public Transform left_foot;
+
+
+    float jump_expiration;
+    [Tooltip("Layer from which the character can jump from")]
+    public LayerMask jumpable;
+
+    float coyote_end;
+    [Tooltip("Time during which the player can still jump after leaving the ground (named after Wil E Coyote)")]
+    public float coyote_time = 0.1f;
+
+    //float jump_end;
+    [Tooltip("Time the player moves upwards while jumping")]
+    public float jump_time;
+    [Tooltip("Time the player floats at the top of the jump arc")]
+    public float hang_time = 0.1f;
+
+    [Tooltip("An audiosource with the clip for jumping loaded")]
+    public AudioSource jump_sound;
+
+    //public TransformPlayer manager;
+
+    float base_gravity;
+
+    float jump_begin;
+    public string state = "falling";
+
+    private void Awake()
+    {
+        base_gravity = body.gravityScale;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Checks if player is touching ground (Collisions are apporximated by a small circle around its "feet")
+        if (Physics2D.OverlapCircle(left_foot.position, 0.01f, jumpable) || Physics2D.OverlapCircle(right_foot.position, 0.01f, jumpable))
+        {
+            beginCoyote();
+        }
+        //Registers a jump input if either the up arrow or w key is pressed
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) //Won't work with controller input TODO
+        {
+            jumpInput(); 
+        }
+    }
+
+    void jump()
+    {
+            jump_sound.Play();
+            OnDust();
+            body.velocity = new Vector2(body.velocity.x, jump_impulse);
+            coyote_end = 0;
+            jump_begin = Time.time;
+            jump_is_buffered = false;
+            state = "jumping";
+    }
+
+    private void FixedUpdate()
+    {
+        //Sets the horizontal velocity to the input times the speed
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        switch (state)
+        {
+            case "grounded": // in coyote time
+                //body.gravityScale = base_gravity;
+                if (jump_is_buffered)
+                {
+                    jump();
+                }
+                break;
+
+            case "jumping":  // going upward
+                body.velocity = new Vector2(body.velocity.x, jump_impulse);
+                if (jumpOverride)
+                {
+                    jump();
+                }
+                //body.gravityScale = base_gravity;
+                jump_is_buffered = false;
+                if (Input.GetAxis("Vertical") <= 0)
+                {
+                    state = "falling";
+                } else if (Time.time > jump_begin + jump_time)
+                {
+                    state = "floating";
+                }
+                break;
+
+            case "floating": // 0 y velocity (potentially gravity off)
+                //body.gravityScale = 0;
+                body.velocity = new Vector2(body.velocity.x, 0);
+                if (Input.GetAxis("Vertical") != 1 || Time.time >jump_begin + jump_time + hang_time)
+                {
+                    state = "falling";
+                }
+                break;
+
+            case "falling":  // set in freefall
+                             //body.gravityScale = base_gravity;
+                if (Input.GetAxis("Vertical") == 1 && Time.time < jump_begin + jump_time + hang_time)
+                {
+                    state = "floating";
+                }
+                body.velocity = new Vector2(body.velocity.x, Mathf.Min(body.velocity.y, 0));
+                //jump_end = 0;
+                break;
+        }
+    }
+    void OnDust()
+    {
+        Instantiate(dust, transform);
+        //dust.GetComponent<ParticleSystem>().Play();
+    }
+
+    //Begins Coyote time
+    void beginCoyote()
+    {
+        state = "grounded";
+        coyote_end = coyote_time + Time.time;
+    }
+
+    //Checks if still within Coyote time
+    bool isGrounded()
+    {
+        return Time.time < coyote_end;
+    }
+
+    //Begins jump buffer
+    void jumpInput()
+    {
+        if (body.velocity.y <= 0 || isGrounded())
+        {
+            jump_is_buffered = true;
+        }
+    }
+/*
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+    	if (other.gameObject.CompareTag("CameraFocus"))
+        {
+            Cam.Focus(other.gameObject.transform, other.transform.localScale.y);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CameraFocus"))
+        {
+            Cam.RemoveView(collision.gameObject.transform);
+            Debug.Log(gameObject);
+        }
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.relativeVelocity.sqrMagnitude > 400)
+        {
+            OnDust();
+        }
+    }
+}
---
+++ Assets/Scripts/Player Logic/squash.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class squash : MonoBehaviour
+{
+    public Rigidbody2D body;
+
+    Vector3 init_transform;
+    public Vector3 squish;
+    public Vector3 stretch;
+
+    Vector3 actual_squish;
+    Vector3 actual_stretch;
+
+
+
+    float y_threshold = 1; 
+    float change_threshold = 10;
+
+    //all in reference to just the y motion
+    float velocity_diff = 0;
+    float last_velocity = 0;
+    float curr_velocity;
+
+    public float elasticity;
+
+    Vector3 target;
+
+    SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        init_transform = transform.localScale;
+        actual_squish = Vector3.Scale(squish, init_transform);
+        actual_stretch = Vector3.Scale(stretch, init_transform);
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        curr_velocity = body.velocity.y;
+        velocity_diff = curr_velocity - last_velocity;
+        last_velocity = curr_velocity;
+
+        if(body.velocity.x < 0)
+        {
+            sprite.flipX = true;
+        } else if(body.velocity.x > 0)
+        {
+            sprite.flipX = false;
+        }
+
+        if(Mathf.Abs(curr_velocity) > y_threshold)
+        {
+            target = actual_stretch;
+        } else if (velocity_diff > change_threshold)
+        {
+            transform.localScale = actual_squish;
+        }
+        else
+        {
+           target = init_transform;
+        }
+
+        transform.localScale = Vector3.Lerp(transform.localScale, target, elasticity);
+    }
+}

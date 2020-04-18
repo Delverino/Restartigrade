e472afba9dd8098cf1d1df3115ab63819100bca9
Ezra Szanton <ezraszanton@yahoo.com>
2020-04-17 23:07:31 -0400

    New spike prototype

---
+++ Assets/Prefabs/Players/Clicker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    public GameObject dieParticles;
+
+
+    private void OnDestroy()
+    {
+        GameObject ob = Instantiate(dieParticles, transform.position, Quaternion.identity);
+        ob.GetComponent<ParticleSystem>().textureSheetAnimation.SetSprite(0, GetComponent<SpriteRenderer>().sprite);
+    }
+}

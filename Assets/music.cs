d906c508c2093e5d23236ad22389e88bcb1c57ae
Ezra Szanton <ezraszanton@yahoo.com>
2020-04-19 20:17:39 -0400

    Implemented music

---
+++ Assets/music.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.FindGameObjectsWithTag("Music").Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
